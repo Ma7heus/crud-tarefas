128148f2f713e548fed1d772a382861a3e9783ec
biasimatheushenrique40 <biasimatheushenrique40@gmail.com>
2024-06-04 16:29:48 +0000

    Criação dos componentes do Form e da Listagem

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { FormComponent } from './form/form.component';
+import { ListComponent } from './list/list.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    { path: 'task-form', component: FormComponent },
+    { path: 'task-list', component: ListComponent }
+];
